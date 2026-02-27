512ef6409863649273686591af926ebcb8a1bd64
Tejas H <tvh1904@gmail.com>
2026-02-27 20:38:01 +0000

    Mecanum Autonomous declaration code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous.java
@@ -4,27 +4,63 @@ This code is meant for the rev robotics mecanum wheels drivertrain.
 
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.IMU;
 
 @com.qualcomm.robotcore.eventloop.opmode.Autonomous
 public class Autonomous extends OpMode {
     // This declares the four motors needed
-    DcMotor frontLeftDrive;
-    DcMotor frontRightDrive;
-    DcMotor backLeftDrive;
-    DcMotor backRightDrive;
+    private DcMotor frontLeftMotor;
+    private DcMotor frontRightMotor;
+    private DcMotor backLeftMotor;
+    private DcMotor backRightMotor;
+
+    private IMU imu;
 
     @Override
     public void init() {
-        frontLeftDrive = hardwareMap.get(DcMotor.class, "front_left_drive");
-        frontRightDrive = hardwareMap.get(DcMotor.class, "front_right_drive");
-        backLeftDrive = hardwareMap.get(DcMotor.class, "back_left_drive");
-        backRightDrive = hardwareMap.get(DcMotor.class, "back_right_drive");
+
+        //Mapping motors
+        frontLeftMotor = hardwareMap.get(DcMotor.class, "front_left_motor");
+        frontRightMotor = hardwareMap.get(DcMotor.class, "front_right_motor");
+        backLeftMotor = hardwareMap.get(DcMotor.class, "back_left_motor");
+        backRightMotor = hardwareMap.get(DcMotor.class, "back_right_motor");
+
+        //Set motor directions
+        frontLeftMotor.setDirection(DcMotor.Direction.REVERSE);
+        backLeftMotor.setDirection(DcMotor.Direction.REVERSE);
+        frontRightMotor.setDirection(DcMotor.Direction.FORWARD);
+        backRightMotor.setDirection(DcMotor.Direction.FORWARD);
+
+        //Set IMU
+        imu = hardwareMap.get(IMU.class, "imu");
+
+        //Set orientation
+        RevHubOrientationOnRobot RevOrientation = new RevHubOrientationOnRobot(
+                RevHubOrientationOnRobot.LogoFacingDirection.UP,
+                RevHubOrientationOnRobot.UsbFacingDirection.FORWARD);
+
+        //Initialise imu
+        imu.initialize(new IMU.Parameters(RevOrientation));
     }
+
+    public void drive(double forward, double strafe, double rotate){
+
+        //Declare the power equations
+        double frontLeftPower = forward + strafe + rotate;
+        double backLeftPower = forward - strafe + rotate;
+        double frontRightPower = forward - strafe - rotate;
+        double backRightPower = forward + strafe - rotate;
+
+        double maxPower = 1.0;
+    }
+
 
     @Override
     public void loop() {
-
+        telemetry.addData("Status", "Running");
+        telemetry.update();
     }
 }
