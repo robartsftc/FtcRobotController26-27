422e9a7c8b8834d9b2feaf2f66c6c284cb3ea173
Tejas H <tvh1904@gmail.com>
2026-02-27 20:19:28 +0000

    Declared the mecanum wheel motors

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous.java
@@ -1,13 +1,26 @@
+/* Hello, all the code in this file was coded by Tejas Heejebu from RoBarts (Team 27941)
+This code is meant for the rev robotics mecanum wheels drivertrain.
+ */
+
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
 
 @com.qualcomm.robotcore.eventloop.opmode.Autonomous
 public class Autonomous extends OpMode {
+    // This declares the four motors needed
+    DcMotor frontLeftDrive;
+    DcMotor frontRightDrive;
+    DcMotor backLeftDrive;
+    DcMotor backRightDrive;
 
     @Override
     public void init() {
-        telemetry.addData("Hello", "World");
+        frontLeftDrive = hardwareMap.get(DcMotor.class, "front_left_drive");
+        frontRightDrive = hardwareMap.get(DcMotor.class, "front_right_drive");
+        backLeftDrive = hardwareMap.get(DcMotor.class, "back_left_drive");
+        backRightDrive = hardwareMap.get(DcMotor.class, "back_right_drive");
     }
 
     @Override
