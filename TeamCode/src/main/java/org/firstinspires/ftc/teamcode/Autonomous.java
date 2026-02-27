7f8179aec279375617ef38d77d8761f774aa2f5e
Tejas H <tvh1904@gmail.com>
2026-02-27 19:44:17 +0000

    Test code commit

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous.java
@@ -1,1 +1,17 @@
 package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+@com.qualcomm.robotcore.eventloop.opmode.Autonomous
+public class Autonomous extends OpMode {
+
+    @Override
+    public void init() {
+        telemetry.addData("Hello", "World");
+    }
+
+    @Override
+    public void loop() {
+
+    }
+}
