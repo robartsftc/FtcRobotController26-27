b9a70063022dcf722d6adc3de53110aea959914d
Tejas H <tvh1904@gmail.com>
2026-03-02 19:37:16 +0000

    Commented code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AprilTagCameraVision.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AprilTagCameraVision.java
@@ -63,6 +63,8 @@ public class AprilTagCameraVision {
         return detectedTags;
     }
 
+
+    //Displays to telemetry
     public void displayDetectionTelemetry(AprilTagDetection detectedId){
         if (detectedId == null){return;}
         if (detectedId.metadata != null) {
